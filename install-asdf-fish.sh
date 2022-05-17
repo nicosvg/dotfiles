d4782dc2861642fd1c1966890eed63b2f4443e61
Nicolas Sauvage <nicolas.sauvage@talan.com>
2022-05-17 14:55:26 +0200

    remove nvchad keep only custom config

---
+++ install-asdf-fish.sh
@@ -0,0 +1,2 @@
+echo "source ~/.asdf/asdf.fish" >> ~/.config/fish/config.fish
+mkdir -p ~/.config/fish/completions; and ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions
---
+++ install-nvchad.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
+stow nvchad
+nvim +'hi NormalFloat guibg=#1e222a' +PackerSync
