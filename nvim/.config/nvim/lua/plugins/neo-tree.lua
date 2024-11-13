8a083b043f541f0b03144b93f34770518ce5ddc6
Nicolas Sauvage <nicolas.sauvage@renaissance.com>
2024-11-13 10:53:30 +0100

    nvim/neo-tree always show .env files

---
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          always_show_by_pattern = {
+            ".env*",
+          },
+        },
+      },
+    },
+  },
+}
