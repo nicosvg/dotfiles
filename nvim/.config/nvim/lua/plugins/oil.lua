b6178d500724942d0db096b827ad6af9761325de
Nicolas Sauvage <nicolas.sauvage@renaissance.com>
2025-01-21 18:35:58 +0100

    [nvim] add oil plugin

---
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,9 @@
+return {
+  "stevearc/oil.nvim",
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = { default_file_explorer = true },
+  -- Optional dependencies
+  dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+}
