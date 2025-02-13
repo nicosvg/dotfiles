34983609efa15dc8b914298ec04e6540fb2f54b4
Nicolas Sauvage <nicolas.sauvage@renaissance.com>
2025-02-13 14:17:54 +0100

    nvim oil config

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -2,7 +2,16 @@ return {
   "stevearc/oil.nvim",
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = { default_file_explorer = true },
+  opts = {
+    default_file_explorer = true,
+  },
+  keys = { {
+    "<leader>o",
+    function()
+      require("oil").open()
+    end,
+    desc = "Oil",
+  } },
   -- Optional dependencies
   dependencies = { { "echasnovski/mini.icons", opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
