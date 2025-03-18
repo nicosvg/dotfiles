3773c09bba50ecedfec3438740a8b42a7d812912
Nicolas Sauvage <nicolas.sauvage@renaissance.com>
2025-03-18 10:45:08 +0100

    nvim: comment fzf

---
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -0,0 +1,11 @@
+return {
+  --   {
+  --     "ibhagwan/fzf-lua",
+  --     cmd = "FzfLua",
+  --     opts = {
+  --       winopts = {
+  --         -- fullscreen = true,
+  --       },
+  --     },
+  --   },
+}
